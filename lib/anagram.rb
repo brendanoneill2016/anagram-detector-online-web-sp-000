f61bbbf7dc81a8f8b1727e3e4e16efd321748b03
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-12-04 11:17:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :word 
+  
+  
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match
+end
+
+def hello 
+end
+
+
+  
+  
+  
+  
+  
+end
