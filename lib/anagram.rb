dfbf404731bd3d3c44ae74566e31983b9d29f350
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-12-04 19:39:02 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,41 @@
 # Your code goes here!
+
+
+class Anagram
+  attr_accessor :word 
+  
+  
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    
+  letters = @word.split("")
+  
+  wrds = array.collect do |w|
+    w.split("")
+  end
+  
+  wrds.each_with_index do |w,index|
+    sorted = w.sort 
+    
+    if sorted == letters.sort
+      return [array[index]]
+    else
+      return []
+    end
+  end
+  
+    
+  end
+
+
+
+  
+  
+  
+  
+  
+end
