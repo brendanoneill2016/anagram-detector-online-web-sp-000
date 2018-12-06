536e677202ccb5a4b5783685d2406e83ef2403b5
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-12-06 09:41:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,42 @@
 # Your code goes here!
+
+require 'pry'
+
+
+class Anagram
+  attr_accessor :word 
+  
+  
+  
+  def initialize("listen")
+    @word = word
+  end
+  
+  def match(["enlists", "google", "inlets", "banana"])
+  
+    
+  letters = @word.split("")
+  
+  wrds = array.collect do |w|
+    w.split("")
+  end
+  
+  wrds.each_with_index do |w,index|
+    sorted = w.sort 
+
+    if sorted == letters.sort
+      return [array[index]]
+    else
+      return []
+    end
+  end
+  
+  binding.pry 
+  
+  end
+
+  
+  
+  
+  
+end
